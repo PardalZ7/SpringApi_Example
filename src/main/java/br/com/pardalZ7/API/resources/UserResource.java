1ac00ca6a16c32f1b120c367690057108696824e
PardalZ7 <alexandrespecian@gmail.com>
2021-12-29 11:38:57 -0300

    class user resource findById

--- src/main/java/br/com/pardalZ7/API/resources/UserResource.java
+++ src/main/java/br/com/pardalZ7/API/resources/UserResource.java
@@ -1,6 +1,8 @@
 package br.com.pardalZ7.API.resources;
 
 import br.com.pardalZ7.API.domain.User;
+import br.com.pardalZ7.API.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -11,10 +13,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class UserResource {
 
+    @Autowired
+    private UserService service;
+
     @GetMapping(value = "/{id}")
     public ResponseEntity<User> findById(@PathVariable Integer id) {
 
-        return ResponseEntity.ok().body(new User(1, "userName", "userEmail", "userPass"));
+        return ResponseEntity.ok().body(service.findById(id));
 
     }
 
