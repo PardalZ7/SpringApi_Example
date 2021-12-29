619aaf4bdf95eac43f274f3bc6a1b717a230a378
PardalZ7 <alexandrespecian@gmail.com>
2021-12-29 11:38:47 -0300

    class user service impl findById

---
+++ src/main/java/br/com/pardalZ7/API/services/impl/UserServiceImpl.java
@@ -0,0 +1,25 @@
+package br.com.pardalZ7.API.services.impl;
+
+import br.com.pardalZ7.API.domain.User;
+import br.com.pardalZ7.API.repositories.UserRepository;
+import br.com.pardalZ7.API.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+    @Autowired
+    private UserRepository repository;
+
+    @Override
+    public User findById(Integer id) {
+
+        Optional<User> user = repository.findById(id);
+        return user.orElse(null);
+
+    }
+
+}
