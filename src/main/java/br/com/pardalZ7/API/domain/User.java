276cf5d59c233ac0d96049887eb42252b56b48d6
PardalZ7 <alexandrespecian@gmail.com>
2021-12-29 10:52:42 -0300

    User class

--- src/main/java/br/com/pardalZ7/API/domain/User.java
+++ src/main/java/br/com/pardalZ7/API/domain/User.java
@@ -6,8 +6,9 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.*;
+import java.util.Objects;
 
-@Entity @Getter @Setter
+@Entity(name = "TB_Users") @Getter @Setter
 @AllArgsConstructor
 @NoArgsConstructor
 public class User {
@@ -20,4 +21,16 @@ public class User {
     private String email;
     private String pass;
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return Objects.equals(id, user.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
 }
