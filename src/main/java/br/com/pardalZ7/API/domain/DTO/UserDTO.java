98e91c7af9879c72d1178ff12f46d1712170bd16
PardalZ7 <alexandrespecian@gmail.com>
2021-12-29 19:13:40 -0300

    Returning DTO object

--- src/main/java/br/com/pardalZ7/API/domain/DTO/UserDTO.java
+++ src/main/java/br/com/pardalZ7/API/domain/DTO/UserDTO.java
@@ -1,19 +1,19 @@
 package br.com.pardalZ7.API.domain.DTO;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.*;
 
-@Setter
+@Setter @Getter
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
-public class UserDTO {
+public class UserDTO{
 
     private Integer id;
     private String name;
     private String email;
+
+    @JsonIgnore
     private String pass;
 
 }
--- src/main/java/br/com/pardalZ7/API/resources/UserResource.java
+++ src/main/java/br/com/pardalZ7/API/resources/UserResource.java
@@ -1,8 +1,11 @@
 package br.com.pardalZ7.API.resources;
 
+import br.com.pardalZ7.API.domain.DTO.UserDTO;
 import br.com.pardalZ7.API.domain.User;
 import br.com.pardalZ7.API.services.UserService;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,12 +17,15 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserResource {
 
     @Autowired
+    private ModelMapper mapper;
+
+    @Autowired
     private UserService service;
 
-    @GetMapping(value = "/{id}")
-    public ResponseEntity<User> findById(@PathVariable Integer id) {
+    @GetMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<UserDTO> findById(@PathVariable Integer id) {
 
-        return ResponseEntity.ok().body(service.findById(id));
+        return ResponseEntity.ok().body(mapper.map(service.findById(id), UserDTO.class));
 
     }
 
