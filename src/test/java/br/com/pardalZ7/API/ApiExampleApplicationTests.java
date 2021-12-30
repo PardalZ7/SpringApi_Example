9656a57df716e585a44bdf6c38cb903898f33ef6
PardalZ7 <alexandrespecian@gmail.com>
2021-12-30 10:24:08 -0300

    test main class

--- src/test/java/br/com/pardalZ7/API/ApiExampleApplicationTests.java
+++ src/test/java/br/com/pardalZ7/API/ApiExampleApplicationTests.java
@@ -7,7 +7,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 class ApiExampleApplicationTests {
 
 	@Test
-	void contextLoads() {
+	void main() {
+		ApiExampleApplication.main(new String[] {});
 	}
 
 }
